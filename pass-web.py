4fe4ed0665ea9f9cc2c2ce3fa0ff1049a5e46108
bsalvagg1 <120699259+bsalvagg1@users.noreply.github.com>
2022-12-17 14:02:23 -0600

    Add files via upload

--- pass-web.py
+++ pass-web.py
@@ -1,17 +1,17 @@
 import streamlit as st
-
+import random
 st.write('''
 # Welcome to Password Generator
 *Get a randomly generated password with a length of your choosing!*
 ''')
 
-length = int(st.text_input('How long would you like your password to be?'))
+length = st.text_input('How long would you like your password to be?')
 
 letter = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
 num = '0123456789'
 char = '!@#$%&*()+?<>'
 password = letter + num + char
-pwd = ''.join(random.sample(password, length))
+pwd = ''.join(random.sample(password, int(length))
 
 st.text_area('Here is your password!')
 st.text_area(pwd)
